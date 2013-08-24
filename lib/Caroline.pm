ab1e1f8fab691dcef2c105195ad5489dcc82b352
tokuhirom <tokuhirom@gmail.com>
2013-08-24 17:28:36 +0900

    Checking in changes prior to tagging of version 0.16.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fea3296..4c4dd37 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Caroline
    
     {{$NEXT}}
    
    +0.16 2013-08-24T08:28:22Z
    +
    +    - Load IO::Handle to call STDOUT->autoflush.
    +      (Reported by KamunagiChiduru++)
    +
     0.15 2013-08-14T03:07:05Z
    
         - fixed deps for CPANTS

--- lib/Caroline.pm
+++ lib/Caroline.pm
@@ -8,7 +8,7 @@ use Text::VisualWidth::PP 0.03 qw(vwidth);
 use Term::ReadKey qw(GetTerminalSize ReadLine ReadKey ReadMode);
 use IO::Handle;
 
-our $VERSION = "0.15";
+our $VERSION = "0.16";
 
 our @EXPORT = qw( caroline );
 
